2e617aa0a84bb96a1bd2f9851e1eb9c990bbee0e
AmarYL1o <115855209+AmarYL1o@users.noreply.github.com>
2025-06-15 19:03:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import KFold
 from AmazonDataset import load_and_split_data
 from trainer import train_and_evaluate
 
-csv_path = "/irip/yueziben_2024/project/AIFramework/dataset/CDs_and_Vinyl.csv"  # 格式：item,user,rate,timestamp
+csv_path = "./dataset/CDs_and_Vinyl.csv"  # 格式：item,user,rate,timestamp
 folds, num_users, num_items, user_pos_items = load_and_split_data(csv_path)
 
 # 5折训练与验证
